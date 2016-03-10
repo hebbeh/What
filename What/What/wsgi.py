bbf8c3ca29a094f056241b763f3bc51b77f9da59
Hebbeh <annpaa91@gmail.com>
2016-03-10 11:48:44 +0100

    settings test

--- What/What/wsgi.py
+++ What/What/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+from . import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "What.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
