83da776231ed27d07f1f225da02cbbfa062823ee
Hebbeh <annpaa91@gmail.com>
2016-03-10 11:57:53 +0100

    new runtime

--- What/What/wsgi.py
+++ What/What/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 from . import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "What.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
