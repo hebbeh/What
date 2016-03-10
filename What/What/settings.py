475b6b9fbb55efbc9340200429436f386cca3f1d
Hebbeh <annpaa91@gmail.com>
2016-03-10 12:24:42 +0100

    Changed INSTALLED_APPS

--- What/What/settings.py
+++ What/What/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Alerter'
+    'What.Alerter'
 ]
 
 MIDDLEWARE_CLASSES = [
